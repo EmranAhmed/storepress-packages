f51615edd918b9c7578d7590b50f014ab38fcbef
Emran Ahmed <emran.bd.08@gmail.com>
2023-12-05 21:57:10 +0600

    icon update

---
+++ packages/icons/build/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "archiveProduct", {
+  enumerable: true,
+  get: function get() {
+    return _archiveProduct["default"];
+  }
+});
+Object.defineProperty(exports, "popup", {
+  enumerable: true,
+  get: function get() {
+    return _popup["default"];
+  }
+});
+Object.defineProperty(exports, "slider", {
+  enumerable: true,
+  get: function get() {
+    return _slider["default"];
+  }
+});
+Object.defineProperty(exports, "storepress", {
+  enumerable: true,
+  get: function get() {
+    return _storepress["default"];
+  }
+});
+var _storepress = _interopRequireDefault(require("./library/storepress"));
+var _popup = _interopRequireDefault(require("./library/popup"));
+var _slider = _interopRequireDefault(require("./library/slider"));
+var _archiveProduct = _interopRequireDefault(require("./library/archive-product"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
---
+++ packages/icons/build/library/storepress.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _primitives = require("@wordpress/primitives");
+/**
+ * External dependencies
+ */
+var _default = exports["default"] = /*#__PURE__*/React.createElement(_primitives.SVG, {
+  xmlns: "http://www.w3.org/2000/svg",
+  viewBox: "0 0 24 24"
+}, /*#__PURE__*/React.createElement("polygon", {
+  fill: "#394fa1",
+  points: "21.5 14.7 7.2 6.5 7.2 6.5 12 3.7 19.2 7.9 19.2 10.6 21.5 12 21.5 6.5 12 1 2.5 6.5 19.1 16.1 16.7 17.5 2.5 9.3 2.5 12 16.8 20.2 21.5 17.5 21.5 14.7"
+}), /*#__PURE__*/React.createElement("polygon", {
+  fill: "#3cb44a",
+  points: "2.5 14.8 2.5 17.5 12 23 14.4 21.6 2.5 14.8"
+}));
