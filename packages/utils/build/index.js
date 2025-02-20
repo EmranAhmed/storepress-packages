7175bd9c85ca465eac98f0c8768db6b63bc02ccc
Emran Ahmed <emran.bd.08@gmail.com>
2025-02-20 16:28:32 +0600

    update utils to 0.3.0

--- packages/utils/build/index.js
+++ packages/utils/build/index.js
@@ -12,56 +12,72 @@ exports.swipeEvent = swipeEvent;
 exports.toCamelCase = toCamelCase;
 exports.toUpperCamelCase = toUpperCamelCase;
 exports.triggerEvent = triggerEvent;
+exports.waitAsync = waitAsync;
+exports.waitSync = waitSync;
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _createForOfIteratorHelper(r, e) { var t = "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (!t) { if (Array.isArray(r) || (t = _unsupportedIterableToArray(r)) || e && r && "number" == typeof r.length) { t && (r = t); var _n = 0, F = function F() {}; return { s: F, n: function n() { return _n >= r.length ? { done: !0 } : { done: !1, value: r[_n++] }; }, e: function e(r) { throw r; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var o, a = !0, u = !1; return { s: function s() { t = t.call(r); }, n: function n() { var r = t.next(); return a = r.done, r; }, e: function e(r) { u = !0, o = r; }, f: function f() { try { a || null == t["return"] || t["return"](); } finally { if (u) throw o; } } }; }
-function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
-function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
 function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
 function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+/**
+ * WeakMap to store plugin instances associated with DOM elements
+ * @type {WeakMap<HTMLElement, any>}
+ */
 var weakMap = new WeakMap();
-function getElement(selector) {
-  // empty
-  if (!selector) {
-    return null;
-  }
 
-  // querySelector
-  if (typeof selector === 'string') {
-    return document.querySelector(selector);
-  }
-
-  // HTMLElement
-  if (selector instanceof window.HTMLElement) {
-    return selector;
-  }
-  return selector;
+/**
+ * Retrieves a single DOM element based on the provided selector.
+ *
+ * @param {string|HTMLElement|null} [selector=null] - CSS selector string or HTMLElement
+ * @returns {HTMLElement|null} The found element or null if not found/invalid
+ *
+ * @example
+ * // Get element by ID
+ * const element1 = getElement('#myId');
+ *
+ * // Pass HTMLElement directly
+ * const element2 = getElement(document.body);
+ *
+ * // Invalid selector returns null
+ * const element3 = getElement(null); // returns null
+ */
+function getElement() {
+  var selector = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+  if (null === selector) return null;
+  if (typeof selector === 'string') return document.querySelector(selector);
+  return selector instanceof HTMLElement ? selector : null;
 }
-function getElements(selectors) {
-  // empty
-  if (!selectors) {
-    return [];
-  }
 
-  // querySelectorAll
-  if (typeof selectors === 'string') {
-    return document.querySelectorAll(selectors);
-  }
-
-  // HTMLElements
-  if (selectors instanceof window.HTMLElement) {
-    return [selectors];
-  }
-  return selectors;
+/**
+ * Gets multiple DOM elements based on the provided selector
+ * @param {string|HTMLElement|NodeList|Array<HTMLElement>} [selectors=[]] - CSS selector string, HTMLElement, or NodeList
+ * @returns {NodeList|Array<HTMLElement>} Array of found elements or empty array
+ */
+function getElements() {
+  var selectors = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+  if (selectors.length === 0) return [];
+  if (typeof selectors === 'string') return document.querySelectorAll(selectors);
+  return selectors instanceof HTMLElement ? [selectors] : selectors;
 }
+
+/**
+ * Converts a string to camelCase
+ * @param {string} string - String to convert
+ * @returns {string} Converted camelCase string
+ */
 function toCamelCase(string) {
   return string.replace(/^([A-Z])|[\s-_](\w)/g, function (match, p1, p2) {
     if (p2) return p2.toUpperCase();
     return p1.toLowerCase();
   });
 }
+
+/**
+ * Converts a string to PascalCase (UpperCamelCase)
+ * @param {string} string - String to convert
+ * @returns {string} Converted PascalCase string
+ */
 function toUpperCamelCase(string) {
   return string.replace(/^([a-z])|[\s-_](\w)/g, function (match, p1, p2) {
     if (p2) return p2.toUpperCase();
@@ -70,11 +86,10 @@ function toUpperCamelCase(string) {
 }
 
 /**
- * Get Option from HTML Attribute
- *
- * @param {Element} element         - HTML Element.
- * @param {string}  attributeName   - Attribute Name
- * @return {Object}                 - Return Object.
+ * Parses options from a data attribute
+ * @param {HTMLElement} element - DOM element containing the attributes
+ * @param {string} attributeName - Name of the attribute to parse
+ * @returns {Object} Parsed options object
  */
 function getOptionsFromAttribute(element, attributeName) {
   var attributeKey = toCamelCase(attributeName);
@@ -114,58 +129,39 @@ function getOptionsFromAttribute(element, attributeName) {
     return {};
   }
 }
+
+/**
+ * Creates plugin instances for selected elements
+ * @param {string|HTMLElement|NodeList} selectors - Elements to create plugins for
+ * @param {Object} options - Plugin options
+ * @param {Function} plugin - Plugin constructor
+ * @returns {Array} Array of plugin instances
+ */
 function createPluginInstance(selectors, options, plugin) {
-  var elements = getElements(selectors);
-  var instances = [];
-  var _iterator = _createForOfIteratorHelper(elements),
-    _step;
-  try {
-    var _loop = function _loop() {
-      var element = _step.value;
-      var instance = weakMap.get(element);
-      if (!weakMap.has(element)) {
-        instance = new plugin(element, options);
-        instance.element = element;
-        instance.destroy = function () {
-          weakMap["delete"](element);
-          triggerEvent(element, 'destroy');
-        };
-        weakMap.set(element, instance);
-      }
-      instances.push(instance);
+  return Array.from(getElements(selectors)).map(function (element) {
+    if (weakMap.has(element)) return weakMap.get(element);
+    var instance = new plugin(element, options);
+    instance.element = element;
+    instance.destroy = function () {
+      weakMap["delete"](element);
+      triggerEvent(element, 'destroy');
     };
-    for (_iterator.s(); !(_step = _iterator.n()).done;) {
-      _loop();
-    }
-  } catch (err) {
-    _iterator.e(err);
-  } finally {
-    _iterator.f();
-  }
-  return instances;
+    weakMap.set(element, instance);
+    return instance;
+  });
 }
+
+/**
+ * Gets existing plugin instances for selected elements
+ * @param {string|HTMLElement|NodeList} selectors - Elements to get plugins for
+ * @returns {Array} Array of plugin instances
+ */
 function getPluginInstance(selectors) {
-  var elements = getElements(selectors);
-  var instances = [];
-  if (elements.length === 0) {
-    return instances;
-  }
-  var _iterator2 = _createForOfIteratorHelper(elements),
-    _step2;
-  try {
-    for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
-      var element = _step2.value;
-      if (weakMap.has(element)) {
-        var instance = weakMap.get(element);
-        instances.push(instance);
-      }
-    }
-  } catch (err) {
-    _iterator2.e(err);
-  } finally {
-    _iterator2.f();
-  }
-  return instances;
+  return Array.from(getElements(selectors)).filter(function (element) {
+    return weakMap.has(element);
+  }).map(function (element) {
+    return weakMap.get(element);
+  });
 }
 
 /**
@@ -193,15 +189,26 @@ function triggerEvent(target, eventType) {
 }
 
 /**
- * @typedef {Function} unregister
+ * @typedef {Object} SwipeOptions
+ * @property {number} [offset=10] - Minimum distance to trigger swipe detection
+ * @property {boolean} [touchOnly=false] - Whether to listen for touch events only
  */
+
 /**
- * Swipe Event.
+ * Creates a swipe event handler for an element with support for both touch and pointer events.
  *
- * @param {Element|Document}                     target     - HTML Element.
- * @param {function(event):void}                 listenerFn - Callback Function Handler
- * @param {{offset: number, touchOnly: boolean}} options    - Options.
- * @return {unregister} - Return `unregister` function for cleanup events.
+ * @param {HTMLElement} target - Element to attach swipe detection to
+ * @param {function(CustomEvent)} listenerFn - Callback function for swipe events
+ * @param {SwipeOptions} [options={}] - Configuration options
+ * @returns {function(): void} Cleanup function to remove event listeners
+ *
+ * @example
+ * const element = document.getElementById('swipeArea');
+ * const cleanup = swipeEvent(element, (e) => {
+ *   if (e.detail.right) console.log('Swiped right!');
+ * });
+ *
+ * // Later: cleanup();
  */
 function swipeEvent(target, listenerFn) {
   var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
@@ -215,6 +222,8 @@ function swipeEvent(target, listenerFn) {
     touchOnly: false
   };
   var settings = _objectSpread(_objectSpread({}, defaults), options);
+  var controller = new AbortController();
+  var signal = controller.signal;
   var start = function start(event) {
     readyToMove = true;
     isMoved = false;
@@ -302,37 +311,89 @@ function swipeEvent(target, listenerFn) {
     readyToMove = false;
   };
   var unregister = function unregister() {
-    target.removeEventListener('touchstart', start);
-    target.removeEventListener('touchmove', move);
-    target.removeEventListener('touchend', end);
-    target.removeEventListener('touchcancel', end);
-    if (!settings.touchOnly) {
-      target.removeEventListener('pointerdown', start);
-      target.removeEventListener('pointermove', move);
-      target.removeEventListener('pointerup', end);
-      target.removeEventListener('pointerleave', end);
-    }
-    target.removeEventListener('swipe', listenerFn);
+    controller.abort();
   };
   var register = function register() {
     target.addEventListener('touchstart', start, {
-      passive: true
+      passive: true,
+      signal: signal
     });
     target.addEventListener('touchmove', move, {
-      passive: true
+      passive: true,
+      signal: signal
     });
     target.addEventListener('touchend', end, {
-      passive: true
+      passive: true,
+      signal: signal
     });
-    target.addEventListener('touchcancel', end);
+    target.addEventListener('touchcancel', end, {
+      signal: signal
+    });
     if (!settings.touchOnly) {
-      target.addEventListener('pointerdown', start);
-      target.addEventListener('pointermove', move);
-      target.addEventListener('pointerup', end);
-      target.addEventListener('pointerleave', end);
+      target.addEventListener('pointerdown', start, {
+        signal: signal
+      });
+      target.addEventListener('pointermove', move, {
+        signal: signal
+      });
+      target.addEventListener('pointerup', end, {
+        signal: signal
+      });
+      target.addEventListener('pointerleave', end, {
+        signal: signal
+      });
     }
-    target.addEventListener('swipe', listenerFn);
+    target.addEventListener('swipe', listenerFn, {
+      signal: signal
+    });
     return unregister;
   };
   return register();
+}
+
+/**
+ * Asynchronously waits for the specified duration without blocking the UI thread.
+ * Uses setTimeout internally to create a non-blocking delay.
+ *
+ * @param {number} milliseconds - The duration to wait in milliseconds
+ * @returns {Promise<void>} A promise that resolves after the specified duration
+ *
+ * @example
+ * // Wait for 2 seconds asynchronously
+ * await waitAsync(2000);
+ * console.log('Continued after 2 seconds without blocking UI');
+ *
+ * @example
+ * // Using in an async function
+ * async function example() {
+ *   console.log('Starting');
+ *   await waitAsync(1000);
+ *   console.log('1 second passed');
+ * }
+ */
+function waitAsync(milliseconds) {
+  return new Promise(function (resolve) {
+    return setTimeout(resolve, milliseconds);
+  });
+}
+
+/**
+ * Synchronously waits for the specified duration, blocking the UI thread.
+ * Uses a busy-wait loop to create a blocking delay.
+ * WARNING: This will freeze the UI during execution.
+ *
+ * @param {number} milliseconds - The duration to wait in milliseconds
+ * @returns {void}
+ *
+ * @example
+ * // Wait for 2 seconds synchronously (NOT RECOMMENDED)
+ * waitSync(2000);
+ * console.log('Continued after 2 seconds of blocking');
+ *
+ * @throws {Error} Implicitly may throw if milliseconds is negative or non-numeric
+ * @warning This function blocks the main thread and should be used with extreme caution
+ */
+function waitSync(milliseconds) {
+  var start = Date.now();
+  while (Date.now() - start < milliseconds) {}
 }
